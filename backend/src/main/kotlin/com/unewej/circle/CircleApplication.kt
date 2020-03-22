9c32980aa009660344218c48b741bcbbdded74a4
Unewe <Unewe@mail.ru>
2020-03-22 21:34:04 +0300

    backend created

---
+++ backend/src/main/kotlin/com/unewej/circle/CircleApplication.kt
@@ -0,0 +1,11 @@
+package com.unewej.circle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CircleApplication
+
+fun main(args: Array<String>) {
+    runApplication<CircleApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/unewej/circle/CircleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.unewej.circle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CircleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
